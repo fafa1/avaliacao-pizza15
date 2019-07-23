f42e0707b0d98b4dc25bd27d20cf3816f211f49e
Fagner <bfagner@hotmail.com>
2019-07-22 23:38:24 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const userRouter = require('./routers/user')
-const port = process.env.PORT
-require('./db/db')
+const port = process.env.PORT || 3000
+//require('./db/db')
 
 const app = express()
 
