dd3033e6f682f5282e25a8eb0e0c0fb9bdcabc86
Fagner <bfagner@hotmail.com>
2019-07-22 23:47:22 -0300

    [UPD] versao-final-producao

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB_URL, {
+const MONGODB_URL = 'mongodb+srv://pizza15Ratings:apiRatings@cluster0-edoag.mongodb.net/pizza15Ratings?retryWrites=true&w=majority'
+mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
 })
