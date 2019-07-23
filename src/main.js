664a016af04610108c658e7bf05aeb77001e27ba
Fagner <bfagner@hotmail.com>
2019-07-23 00:53:03 -0300

    star-rating

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import StarRating from 'vue-star-rating'
 import { VueStars } from "vue-stars"
 import BootstrapVue from 'bootstrap-vue'
 
