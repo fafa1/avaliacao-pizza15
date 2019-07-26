42cc955de8b031f96ce5d069222ce339f95b40b2
Fagner <bfagner@hotmail.com>
2019-07-26 00:27:03 -0300

    [UPD] rota de home

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,11 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/',
       name: sobre,
